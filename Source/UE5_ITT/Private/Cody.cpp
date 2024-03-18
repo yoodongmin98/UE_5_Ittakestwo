93266e0e6d9967e111bed4d275db97fa32a8c4f3
yoodongmin98 <117648173+yoodongmin98@users.noreply.github.com>
2024-03-18 22:28:54 +0900

    유동민_Add : Player제어 추가

---
+++ Source/UE5_ITT/Private/Cody.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Cody.h"
+
+// Sets default values
+ACody::ACody()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACody::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACody::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACody::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(TEXT("Player_MoveFB"), this, &ACody::Cody_MoveFB);
+	PlayerInputComponent->BindAxis(TEXT("Player_MoveLR"), this, &ACody::Cody_MoveLR);
+	PlayerInputComponent->BindAxis(TEXT("Player_LookUD"), this, &ACody::Cody_LookUD);
+	PlayerInputComponent->BindAxis(TEXT("Player_LookLR"), this, &ACody::Cody_LookLR);
+}
+
+
+//////////////////////////////캐릭터 이동관련///////////////////////////
+void ACody::Cody_MoveFB(float _AxisValue)
+{
+	AddMovementInput(GetActorForwardVector() * _AxisValue);
+}
+void ACody::Cody_MoveLR(float _AxisValue)
+{
+	AddMovementInput(GetActorRightVector() * _AxisValue);
+}
+
+void ACody::Cody_LookUD(float _AxisValue)
+{
+	AddControllerPitchInput(_AxisValue * UpRotationSpeed * GetWorld()->GetDeltaSeconds());
+}
+
+void ACody::Cody_LookLR(float _AxisValue)
+{
+	AddControllerYawInput(_AxisValue * RightRotationSpeed * GetWorld()->GetDeltaSeconds());
+}
