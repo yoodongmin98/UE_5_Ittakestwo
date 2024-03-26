e8c6e9a9a5a96df5f7267ac4ad63d1ecc267db06
SujeongLee <audrbekf@gmail.com>
2024-03-26 18:41:07 +0900

    이수정 feat : 메인UI 생성

---
+++ Source/UE5_ITT/Private/TestLobbyWidget.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TestLobbyWidget.h"
+
+void UTestLobbyWidget::SetUser1Cody() const
+{
+}
+
+void UTestLobbyWidget::SetReleaseUser1FromCody() const
+{
+
+}
