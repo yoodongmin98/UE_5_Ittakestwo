f6a42265d79de482b9d5facd6b9eeb9815783d37
Dongseok <zest2700@gmail.com>
2024-04-18 16:08:17 +0900

    허동석_fix : 리플리케이트 관련내용 수정

--- Source/UE5_ITT/Private/EnemyMoonBaboon.cpp
+++ Source/UE5_ITT/Private/EnemyMoonBaboon.cpp
@@ -14,13 +14,13 @@ AEnemyMoonBaboon::AEnemyMoonBaboon()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	/*GetCapsuleComponent()->DestroyComponent();
-	GetCharacterMovement()->DestroyComponent();*/
+	if (true == HasAuthority())
+	{
+		bReplicates = true;
+		SetReplicateMovement(true);
 
-	SceneComp = CreateDefaultSubobject<USceneComponent>(TEXT("Root Component"));
-	SetRootComponent(SceneComp);
-	GetArrowComponent()->SetupAttachment(SceneComp);
-	GetMesh()->SetupAttachment(SceneComp);
+		SetupComponent();
+	}
 }
 
 // Called when the game starts or when spawned
@@ -31,10 +31,15 @@ void AEnemyMoonBaboon::BeginPlay()
 	// 네트워크 권한을 확인하는 코드
 	if (true == HasAuthority())
 	{
-		// 서버와 클라이언트 모두에서 변경사항을 적용할 도록 하는 코드입니다.
-		SetReplicates(true);
-		SetReplicateMovement(true);
 	}
+}
+
+void AEnemyMoonBaboon::SetupComponent()
+{
+	SceneComp = CreateDefaultSubobject<USceneComponent>(TEXT("Root Component"));
+	SetRootComponent(SceneComp);
+	GetArrowComponent()->SetupAttachment(SceneComp);
+	GetMesh()->SetupAttachment(SceneComp);
 }
 
 // Called every frame
@@ -53,6 +58,5 @@ void AEnemyMoonBaboon::Tick(float DeltaTime)
 void AEnemyMoonBaboon::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
 
