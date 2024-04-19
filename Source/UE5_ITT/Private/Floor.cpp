5c1af5276275db6fcb400440e10739eeedd4ed9d
동찬 황 <shidge98@gmail.com>
2024-04-19 18:12:05 +0900

    황동찬 : feat : FX Electric 추가

--- Source/UE5_ITT/Private/Floor.cpp
+++ Source/UE5_ITT/Private/Floor.cpp
@@ -12,7 +12,6 @@ AFloor::AFloor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	SetupFsm();
 
 	Tags.Add(FName("Floor"));
 
@@ -22,6 +21,7 @@ AFloor::AFloor()
 		// 서버와 클라이언트 모두에서 변경사항을 적용할 도록 하는 코드입니다.
 		bReplicates = true;
 		SetReplicateMovement(true);
+		SetupFsm();
 	}
 }
 
---
+++ Source/UE5_ITT/Public/FxElectric.h
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FxElectric.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_ITT_API AFxElectric : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AFxElectric(const FObjectInitializer& ObjectInitializer);
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	enum class Fsm
+	{
+		None,
+		Wait,
+		End,
+	};
+
+public:
+	UFUNCTION(NetMulticast,Reliable)
+	void NiagaraToggle();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	class UFsmComponent* FsmComp = nullptr;
+	
+	UPROPERTY(EditAnywhere)
+	class UNiagaraComponent* NiagaraComp = nullptr;
+
+	UPROPERTY(EditAnywhere)
+	bool bDelay = false;
+
+	void SetupFsm();
+
+};
