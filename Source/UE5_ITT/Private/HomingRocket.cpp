18f357af587834bb2bc5f5ad968a2e6552b70579
Dongseok <zest2700@gmail.com>
2024-05-07 20:51:18 +0900

    허동석_feat : HomingRocket 나이아가라 컴포넌트 온오프 함수 변경

--- Source/UE5_ITT/Private/HomingRocket.cpp
+++ Source/UE5_ITT/Private/HomingRocket.cpp
@@ -155,7 +155,7 @@ void AHomingRocket::SetupFsmComponent()
 			// 대기상태로 전환 될 때, 중력 온시켜서 바닥에 떨어지는 거처럼 만들고. 
 			RocketMeshComp->SetSimulatePhysics(true);
 			RocketMeshComp->SetEnableGravity(true);
-			Multicast_ActivateFireEffectComponent(false);
+			Multicast_ActivateFireEffectComponent();
 		},
 
 		[this](float DT)
@@ -219,7 +219,6 @@ void AHomingRocket::SetupFsmComponent()
 			if (nullptr != OverlapActor)
 			{
 				bIsPlayerEquip = true;
-
 				RocketMeshComp->SetSimulatePhysics(false);
 				RocketMeshComp->SetEnableGravity(false);
 				RocketMeshComp->AttachToComponent(SceneComp, FAttachmentTransformRules::KeepRelativeTransform);
@@ -233,6 +232,7 @@ void AHomingRocket::SetupFsmComponent()
 					this->SetOwner(OverlapActor);
 					
 					EnablePlayerFlying();
+					Multicast_ActivateFireEffectComponent();
 				}
 			}
 		},
@@ -337,9 +337,9 @@ void AHomingRocket::Tick(float DeltaTime)
 	}
 }
 
-void AHomingRocket::Multicast_ActivateFireEffectComponent_Implementation(const bool bIsActivate)
+void AHomingRocket::Multicast_ActivateFireEffectComponent_Implementation()
 {
-	FireEffectComp->SetActive(bIsActivate, true);
+	FireEffectComp->ToggleActive();
 }
 
 bool AHomingRocket::IsMaxFloorDistance()
@@ -458,7 +458,7 @@ void AHomingRocket::DestroyRocket()
 // Fly 활성 
 void AHomingRocket::EnablePlayerFlying()
 {
-	//OverlapActor->TestFunction();
+	// OverlapActor->TestFunction();
 }
 
 // Fly 비활성
