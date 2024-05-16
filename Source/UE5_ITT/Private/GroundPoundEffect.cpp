eaebf7541207ff1f459681160d4d4895620de60b
Dongseok <zest2700@gmail.com>
2024-05-16 15:56:16 +0900

    허동석_feat : 그라운드 이펙트 메시 추가 및 데미지 적용, remove : 미사용 파일 삭제

--- Source/UE5_ITT/Private/GroundPoundEffect.cpp
+++ Source/UE5_ITT/Private/GroundPoundEffect.cpp
@@ -2,7 +2,12 @@
 
 
 #include "GroundPoundEffect.h"
+#include "Net/UnrealNetwork.h"
+#include "Components/StaticMeshComponent.h"
 #include "NiagaraComponent.h"
+#include "PlayerBase.h"
+
+
 
 // Sets default values
 AGroundPoundEffect::AGroundPoundEffect()
@@ -18,6 +23,9 @@ AGroundPoundEffect::AGroundPoundEffect()
 		SceneComp = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComponent"));
 		SetRootComponent(SceneComp);
 
+		StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComponent"));
+		StaticMeshComp->SetupAttachment(SceneComp);
+		
 		GroundPoundEffectComp = CreateDefaultSubobject<UNiagaraComponent>(TEXT("GroundPoundEffectComponent"));
 		GroundPoundEffectComp->SetupAttachment(SceneComp);
 	}
@@ -27,10 +35,72 @@ AGroundPoundEffect::AGroundPoundEffect()
 void AGroundPoundEffect::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (true == HasAuthority())
+	{
+		if (nullptr != StaticMeshComp)
+		{
+			StaticMeshComp->OnComponentBeginOverlap.AddDynamic(this, &AGroundPoundEffect::OnOverlapBegin);
+			StaticMeshComp->OnComponentEndOverlap.AddDynamic(this, &AGroundPoundEffect::OnOverlapEnd);
+		}
+	}
 }
 
 // Called every frame
 void AGroundPoundEffect::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	if (true == HasAuthority())
+	{
+		if (true == bIsPlayerOverlap)
+		{
+			// OverlapPlayer->AttackPlayer(DamageToPlayer);
+		}
+	}
 }
+
+
+void AGroundPoundEffect::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AGroundPoundEffect, bIsPlayerOverlap);
+}
+
+void AGroundPoundEffect::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (nullptr != OtherActor)
+	{
+		if (true == OtherActor->ActorHasTag(TEXT("Player")))
+		{
+			if (true == OtherActor->HasAuthority())
+			{
+				OtherComp->GetName();
+				
+				UE_LOG(LogTemp, Display, TEXT("Otherdasdfdasadfa f"));
+				UE_LOG(LogTemp, Display, TEXT("Player Overlap true"));
+				bIsPlayerOverlap = true;
+				OverlapPlayer = Cast<APlayerBase>(OtherActor);
+			}
+		}
+	}
+}
+
+void AGroundPoundEffect::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (nullptr != OtherActor)
+	{
+		if (true == OtherActor->ActorHasTag(TEXT("Player")))
+		{
+			if (true == OtherActor->HasAuthority())
+			{
+				UE_LOG(LogTemp, Display, TEXT("Player Overlap false"));
+				bIsPlayerOverlap = false;
+				OverlapPlayer = nullptr;
+			}
+			
+		}
+	}
+}
+
--- Source/UE5_ITT/Public/GroundPoundEffect.h
+++ Source/UE5_ITT/Public/GroundPoundEffect.h
@@ -18,14 +18,34 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 private:
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
 	UPROPERTY(EditDefaultsOnly, Category = "Component")
 	class USceneComponent* SceneComp = nullptr;
 
 	UPROPERTY(EditDefaultsOnly, Category = "Component")
 	class UNiagaraComponent* GroundPoundEffectComp = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Component")
+	class UStaticMeshComponent* StaticMeshComp = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, Replicated)
+	bool bIsPlayerOverlap = false;
+
+	UPROPERTY(EditDefaultsOnly)
+	class APlayerBase* OverlapPlayer = nullptr;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 DamageToPlayer = 1;
 };
